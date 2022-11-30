40032ef5f240d4b65e8afc5a33c1ae7a61d4da86
Fazer-y <2388737225@qq.com>
2022-11-30 21:59:51 +0800

    实验17

---
+++ cgExperiment17LightModel/mainwindow.h
@@ -0,0 +1,44 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPainter>
+#include <QColorDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void paintEvent(QPaintEvent*);
+
+private slots:
+    void on_checkBox_lightEnable_stateChanged(int arg1);
+
+    void on_checkBox_doubleFace_stateChanged(int arg1);
+
+    void on_checkBox_infinete_stateChanged(int arg1);
+
+    void on_checkBox_global_stateChanged(int arg1);
+
+    void on_pushButton_diffuse_clicked();
+
+    void on_pushButton_specular_clicked();
+
+    void on_pushButton_ambient_clicked();
+
+    void on_comboBox_material_activated(int index);
+
+private:
+    Ui::MainWindow *ui;
+    QColor diffuseColor;
+    QColor specularColor;
+    QColor ambientColor;
+};
+#endif // MAINWINDOW_H
---
+++ cgExperiment17LightModel/widgetdraw.h
@@ -0,0 +1,44 @@
+﻿#ifndef WIDGETDRAW_H
+#define WIDGETDRAW_H
+
+#include <QWidget>
+#include <QColor>
+#include <QGLWidget>
+#include <QtOpenGL/glut.h>
+
+class widgetDraw : public QGLWidget
+{
+    Q_OBJECT
+
+public:
+    explicit widgetDraw(QWidget *parent = nullptr);
+
+private:
+    bool lightEnable;
+    bool doubleFaceEnable;
+    bool infiniteEnable;
+    bool globalAmbientEnable;
+    QColor diffuseColor;
+    QColor specularColor;
+    QColor ambientColor;
+    int comboIndex;
+public:
+    void setLightEnable(bool flag);
+    void setDoubleFaceEnable(bool flag);
+    void setInfiniteEnable(bool flag);
+    void setGlobalAmbientEnable(bool flag);
+    void setDiffuseColor(QColor clr);
+    void setSpecularColor(QColor clr);
+    void setAmbientColor(QColor clr);
+    void setComboIndex(int n);
+    void paintGL();
+    void colorChange(QColor clr, int mode);
+    void render(int index);
+    void initLight();
+    void DrawScene();
+
+signals:
+
+};
+
+#endif // WIDGETDRAW_H
