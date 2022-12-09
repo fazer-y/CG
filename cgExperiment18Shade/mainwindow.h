ddd5c009f4fa04a7825fdf184f98d3fa9a6dfd78
Fazer-y <2388737225@qq.com>
2022-12-09 20:53:05 +0800

    实验18、19、20

---
+++ cgExperiment18Shade/mainwindow.h
@@ -0,0 +1,29 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtOpenGL/glut.h>
+#include <QPainter>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void paintEvent(QPaintEvent*);
+
+private slots:
+    void on_action_lineFrame_triggered();
+
+    void on_action_surface_triggered();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
---
+++ cgExperiment18Shade/widgetdraw.h
@@ -0,0 +1,40 @@
+﻿#ifndef WIDGETDRAW_H
+#define WIDGETDRAW_H
+
+#include <QWidget>
+#include <QGLWidget>
+#include <glut.h>
+#include <QTime>
+#include <QMouseEvent>
+#include  <QPainter>
+typedef GLfloat Point3[3];
+typedef GLfloat Point4[4];
+typedef GLfloat Matrix[16];
+
+class widgetDraw : public QGLWidget
+{
+    Q_OBJECT
+public:
+    explicit widgetDraw(QWidget *parent = nullptr);
+    void ground();
+    void setLight();
+    void GetShadeMatrix(Point4 lightPos, Point4 planeEquation, Matrix desMatrix);
+    void setLineFrame();
+    void setSurface();
+    void paintGL();
+    void timerEvent(QTimerEvent*);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+
+signals:
+
+private:
+    GLUquadricObj* pSphere;
+    float m_angle;
+    bool isLine;
+    bool isTimer;
+    int id;
+
+};
+
+#endif // WIDGETDRAW_H
